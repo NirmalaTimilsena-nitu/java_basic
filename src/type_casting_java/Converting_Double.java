11dae5461642a92e1ab750b82321b7faf26a89a5
NirmalaTimilsena-nitu <anitim26@gmail.com>
2025-02-18 07:19:03 +0545

    Update with data handling and looping statement

---
+++ src/type_casting_java/Converting_Double.java
@@ -0,0 +1,15 @@
+package type_casting_java;
+
+//Implicit Type Casting/ Automatic/Upcasting
+public class Converting_Double {
+	public static void main(String[] args) {
+		// create int type variable
+		int num = 10;
+		System.out.println("The integer value: " + num);
+
+		// convert into double type
+		double data = num;
+		System.out.println("The double value: " + data);
+	}
+
+}
